575bec4280a0db1d89f4ba199a801e9c97d617f6
MochIsrin068 <isrin068@gmail.com>
2023-06-22 10:31:05 +0800

    config for prod env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,10 +4,26 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("PGHOST", "127.0.0.1"),
       port: env.int("PGPORT", 5931),
-      database: env("PGDATABASE", "railway"),
+      // database: env("PGDATABASE", "railway"),
+      database: env("PGDATABASE", "strapi"),
       user: env("PGUSER", "postgres"),
       password: env("PGPASSWORD", "password"),
       ssl: env.bool(true),
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("PGHOST", "127.0.0.1"),
+//       port: env.int("PGPORT", 5431),
+//       database: env("PGDATABASE", "strapi"),
+//       user: env("PGUSER", "strapi"),
+//       password: env("PGPASSWORD", "password"),
+//       ssl: env.bool(true),
+//     },
+//     pool: { min: 0 },
+//   },
+// });
---
+++ config/env/production/server.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
+});
